f5f940c9db8922c835bb545e72012226b712832b
Da-Ronja <70214836+Da-Ronja@users.noreply.github.com>
2021-02-23 15:04:03 +0100

    Sass byggt upp alla viewer komponenter

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,28 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Products from '../views/Products.vue'
+import Checkout from '../views/Checkout.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/products',
+    name: 'Products',
+    component: Products
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/checkout',
+    name: 'Checkout',
+    component: Checkout
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
