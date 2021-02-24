964a12db96bd995c2d9835ca13e832cfbdcacb53
Stefan Sjöman <stefan.sjoman@hotmail.com>
2021-02-24 12:00:49 +0100

    added fonts and img folder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,9 +7,18 @@
 </template>
 
 <style lang="scss" scoped>
+  @font-face {
+    font-family: 'font1' ;
+    src: url('./assets/fonts/Play/Play-Regular.ttf');
+  }
+  @font-face {
+    font-family: 'font2';
+    src: url('./assets/fonts/Open_Sans/OpenSans-Regular.ttf');
+  }
   $color1: white;
   $color2: black;
   $accent-color: rgb(255, 152, 57);
   $yes-green: rgb(12, 145, 0);
   $no-red: red;
+
 </style>
