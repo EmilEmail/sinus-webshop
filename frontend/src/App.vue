f9e142fd070bbf7bcfe7aa79bbe3143f3e546dc6
email-90 <70515786+email-90@users.noreply.github.com>
2021-02-24 11:37:01 +0100

    update rätt router link

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/checkout">About</router-link>
+    <router-link to="/">Products</router-link> |
+    <router-link to="/checkout">Checkout</router-link>
     <router-view/>
   </div>
 </template>
