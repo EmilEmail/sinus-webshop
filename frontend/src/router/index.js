4096e10fc7eee08de30977ab17f8580b9e1dec34
Da-Ronja <70214836+Da-Ronja@users.noreply.github.com>
2021-02-24 11:01:51 +0100

    Started up Comp View App.vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Products from '../views/Products.vue'
+import Checkout from '../views/Checkout.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Products',
+    component: Products
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/checkout',
+    name: 'Checkout',
+    component: Checkout
+  },
 ]
 
 const router = new VueRouter({
