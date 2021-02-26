58157de135e3fd59bd6e4305d81ee5bd8791e4f9
Da-Ronja <70214836+Da-Ronja@users.noreply.github.com>
2021-02-26 11:12:28 +0100

    ProfileComp vue grejer oc grid

--- frontend/src/components/ProfileComp.vue
+++ frontend/src/components/ProfileComp.vue
@@ -1,48 +1,46 @@
 <template>
   <div class="profile-comp-wrapper">
-    <div class="profile-comp-profile">
+    <div class="profile-comp-profile profil-comp-grid">
         <h2>Profil</h2>
-        <img src="../assets/images/Profilimg.png" alt="profil image">
-      <div class="profile-comp-costumer">
-        <p>kundnamn</p>
-        <hr>
-        <div class="profile-comp-grid-costumer">
-          <p class="col-1">KundNr:</p>
-          <p class="col-2">123456778</p>
+        <img src="../assets/images/Profilimg.png" alt="profil image" class="col-1">
+        <div class="col-profile">
+        <div class="col-borders">
+          <p>{{ user.name }}</p>
+        </div>
+          <p class="col-2">KundNr:</p>  
+          <p class="col-5">123456778</p>
           
-          <p class="col-1">Email:</p>   
-          <p class="col-2">kund@kundson.se</p>
+          <p class="col-2">Email:</p>   
+          <p class="col-5">{{ user.email }}</p>
           
-          <p class="col-1">Address:</p>
-          <div class="col-2 no-padding">
-            <p class="col-2">körtelvägen 781</p>
-            <p class="col-2">12344 enskede</p>
+          <p class="col-2">Address:</p>
+          <div class="col-5">
+            <p>{{ user.address.street }}</p>
+            <p>{{ user.address.zip + user.address.city}}</p>
           </div>
         </div>
-      </div>
+
     </div>
 
     <div class="profil-comp-order-history">
       <h2>Orderhistorik</h2>
-      <div class="profil-comp-order-history-grid">
+      <div class="profil-comp-grid">
         <p class="col-1">Ordernummer</p>
         <p class="col-2">Datum</p>
         <p class="col-4">Pris</p>
       </div>
       <ul class="profil-comp-order-history-border">
-        <li class="profil-comp-order-history-grid">
-          <p class="col-1">7123687213</p>
-          <p class="col-2">20-20</p>
+        <li v-for="(order, index) in orderHistery" :key="index" class="profil-comp-grid">
+          <p class="col-1">{{ order.orderNr }}</p>
+          <p class="col-2">{{ order.date }}</p>
           <button class="col-3">X</button>
-          <p class="col-4">399:-</p>
-        </li>
-        <li class="profil-comp-order-history-grid">
-          <p class="col-1">7123687213</p>
-          <p class="col-2">20-20</p>
-          <button class="col-3">X</button>
-          <p class="col-4">399:-</p>
+          <p class="col-4">{{ order.price }}:-</p>
         </li>
       </ul>
+      <div class="profil-comp-grid">
+        <p class="col-1">Summa</p>
+        <p class="col-4">{{ totalSum }}:-</p>
+      </div>
     </div>
 
   </div>
@@ -50,50 +48,63 @@
 
 <script>
 export default {
-
+  data() {
+    return{
+      user:{
+        email: "customer@example.com",
+        name: "David Lundholm",
+        role: "customer",
+        address: {
+          street: "Tokitokvägen 4",
+          zip: "123 46",
+          city: "Tokbergaskogen"
+        }
+      },
+      orderHistery: [{
+        orderNr: 123456789081,
+        date: "20-20",
+        price: 699
+      },
+      {
+        orderNr: 123456927408,
+        date: "21-20",
+        price: 889
+      },
+      {
+        orderNr: 12345694523,
+        date: "22-20",
+        price: 2599
+      }]
+    }
+  },
+  computed: {
+    totalSum: function() {
+      let total = 0;
+      this.orderHistery.forEach(item => {
+        total += item.price;
+      });
+      return total;
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../assets/css/colors.scss';
-
+.profile-comp-wrapper{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 .profile-comp-profile {
-  display: grid;
-  grid-template-columns: 25% 75%;
-  grid-template-rows: 25% 75%;
   width: 592px;
   height: 272px;
   background-color: $color2;
   color: $color1;
   border: 16px solid $color2;
-  margin-bottom: 16px; //Detta ska bort
+  margin-bottom: 16px; //Detta ska bort eller ändras
   img {
-    grid-column: 1;
-    grid-row: 2;
-  }
-  .profile-comp-costumer {
-    grid-column: 2;
-    grid-row: 2/3;
-    .no-padding {
-      p {
-        padding: 0;
-      }
-    }
-    hr {
-      margin-bottom: 16px;
-    }
-  }
-}
-.profile-comp-grid-costumer {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: auto;
-  .col-1 {
-    grid-column: 1;
-  }
-  .col-2 {
-    grid-column: 2;
-    justify-self: end;
+    grid-row: 2/5;
   }
 }
 .profil-comp-order-history {
@@ -102,39 +113,50 @@ export default {
   background-color: $color2;
   color: $color1;
   border: 16px solid $color2;
-  margin-bottom: 16px; //Detta ska bort
+  margin-bottom: 16px; //Detta ska bort eller ändras
 }
 h2 {
-  grid-column: 1;
+  grid-column: 1/9;
   grid-row: 1;
   font-family: 'font1';
   font-size: 40px;
 }
 p {
-  padding-bottom: 8px;
   font-family: 'font2';
   font-size: 16px;
 }
-.profil-comp-order-history-grid {
+.profil-comp-grid {
   display: grid;
-  grid-template-columns: 64px 64px 64px 64px 64px 64px 64px;
+  grid-template-columns: repeat(auto-fill, 64px);
+  grid-template-rows: auto;
   grid-gap: 16px;
-  .col-1 {
-    grid-column: 1/3;
-  }
-  .col-2 {
-    grid-column: 3/5;
-  }
-  .col-3 {
-    grid-column: 6;
-  }
-  .col-4 {
-    grid-column: 7/9;
-    margin-left: auto;
-  }
 }
-  .profil-comp-order-history-border{
-    border-top: 2px solid $color1;
-    border-bottom: 2px solid $color1;
-  }
+.col-1 {
+  grid-column: 1/3;
+}
+.col-2 {
+  grid-column: 3/5;
+}
+.col-3 {
+  grid-column: 6;
+}
+.col-4 {
+  grid-column: 7/9;
+  text-align: end;
+}
+.col-5 {
+  grid-column: 5/9;
+  text-align: end;
+}
+.col-profile{
+  grid-column: 3/9;
+}
+.col-borders{
+  grid-column: 1/7;
+  border-bottom: white 2px solid;
+}
+.profil-comp-order-history-border{
+  border-top: 2px solid $color1;
+  border-bottom: 2px solid $color1;
+}
 </style>
