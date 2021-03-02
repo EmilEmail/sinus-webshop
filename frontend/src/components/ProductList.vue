bcd7d38fbfbaf42071860599778cd07f973f24e5
Da-Ronja <70214836+Da-Ronja@users.noreply.github.com>
2021-03-02 12:19:17 +0100

    Knappar till pagination
    funkar ej

--- frontend/src/components/ProductList.vue
+++ frontend/src/components/ProductList.vue
@@ -1,40 +1,63 @@
 <template>
    <div class="product-list-grid-wrapper-top">
-    <h2>Brädor</h2>
-      <ul>
-        <li 
-          v-for="(product,index) in product.boards" 
-          :key="index"
-          :product='product'
-          @click="openModal(product)"
-        >
-          <ProductCard v-bind:product="product" /> 
-        </li>
-      </ul>
+    <div>
+      <h2>Brädor</h2>
+        <ul>
+          <li 
+            v-for="(product,index) in product.boards" 
+            :key="index"
+            :product='product'
+            @click="openModal(product)"
+          >
+            <ProductCard v-bind:product="product" /> 
+          </li>
+          <button :disabled="pageNumber === 1" @click="prevPage" >
+            Previous
+          </button>
+          <button :disabled="pageNumber < pages.length" @click="nextPage">
+            Next
+          </button>
+        </ul>
+      </div>
 
-    <h2>Kläder</h2>
-      <ul>
-        <li 
-          v-for="(product,index) in product.clothes" 
-          :key="index"
-          :product='product'
-          @click="openModal(product)"
-        >
-          <ProductCard v-bind:product="product" /> 
-      </li>
-    </ul>
+      <div>
+        <h2>Kläder</h2>
+          <ul>
+            <li 
+              v-for="(product,index) in product.clothes" 
+              :key="index"
+              :product='product'
+              @click="openModal(product)"
+            >
+              <ProductCard v-bind:product="product" /> 
+            </li>
+            <button :disabled="pageNumber === 1" @click="prevPage" >
+              Previous
+            </button>
+            <button :disabled="pageNumber < pages.length" @click="nextPage">
+              Next
+            </button>
+          </ul>
+        </div>
 
-    <h2>Tillbehör</h2>
-      <ul>
-        <li 
-          v-for="(product,index) in product.wheels" 
-          :key="index"
-          :product='product'
-          @click="openModal(product)"
-        >
-          <ProductCard v-bind:product="product" /> 
-      </li>
-    </ul>
+    
+        <h2>Tillbehör</h2>
+          <ul>
+            <li 
+              v-for="(product,index) in product.wheels" 
+              :key="index"
+              :product='product'
+              @click="openModal(product)"
+            >
+              <ProductCard v-bind:product="product" /> 
+            </li>
+            <button :disabled="pageNumber === 1" @click="prevPage" >
+              Previous
+            </button>
+            <button :disabled="pageNumber < pages.length" @click="nextPage">
+              Next
+            </button>
+          </ul>
 
     <ProductModal 
       v-if="showModal" 
@@ -55,7 +78,10 @@ export default {
       showModal: false,
       closeModal: true,
       productItem: {},
-
+      
+      pageNumber: 1,//k
+			perPage: 1,//k
+			pages: [],//k
     }
   },
   components: {
@@ -68,6 +94,15 @@ export default {
     this.product.wheels = this.$store.getters.products.filter(pro => pro.category == "wheels");
   },
   computed: {
+    // productBoards: function() {
+    //   return this.$store.getters.products.filter(pro => pro.category == "board");
+    // },
+    // productClothes: function() {
+    //   return this.$store.getters.products.filter(pro => pro.category == "clothes");
+    // },
+    // productWheels: function() {
+    //   return this.$store.getters.products.filter(pro => pro.category == "wheels");
+    // },
   },
   methods: {
     openModal(item){
@@ -76,8 +111,26 @@ export default {
     },
     toCloseModal() {
       this.showModal = false
+    },
+    paginate (products) {     //k
+			let pageNumber = this.pageNumber;
+			let perPage = this.perPage;
+			let start = (pageNumber * perPage) - perPage;
+			let end = (pageNumber * perPage);
+			return  products.slice(start, end);
+		},
+    nextPage(){ //k
+     this.pageNumber++;
+    },
+    prevPage(){   //k
+      this.pageNumber--;
+    },
+    pageCount(){    //k
+      let l = this.products.length,
+          s = this.perPage;
+      return Math.ceil(l/s);
     }
-  },
+  }
 }
 </script>
 
